d59a6145b4e07761cf66d5b2b0d76ee4ef0ffb21
Ruchir Rathore <rathoreruchir7@gmail.com>
2020-04-10 03:48:16 +0530

    Single Page Applications 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import {Card, CardImg, CardText, CardImgOverlay, CardBody, CardTitle} from 'reactstrap';
-
+import {Card, CardImg, CardText, CardImgOverlay, CardBody, CardTitle,Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 	
 	function RenderDish({dish})
 	{	return(
 		
-					<Card>
-						<CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
+				<Card>
+					<CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
 
-					</Card>
+				</Card>
 				);
 	}
 
@@ -51,13 +51,26 @@ import {Card, CardImg, CardText, CardImgOverlay, CardBody, CardTitle} from 'reac
 		return(
 			
 			<div className ="container">
+
+				<div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                
 				<div className="row">
 				<div className="col-12 col-md-5 m-1">	
 					<RenderDish dish={props.dish} />
 				</div>
 
 				<div className="col-12 col-md-5 m-1">
-				   <RenderComments comments = {props.dish.comments} />
+				   <RenderComments comments = {props.comments} />
 				</div>
 			</div>
 			</div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -21,7 +21,7 @@ class Main extends Component {
       leaders: LEADERS
     };
   }
-  
+ 
    render() {
     const HomePage = () =>{
     return( <Home 
@@ -30,6 +30,14 @@ class Main extends Component {
               leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />);
     }
+
+     const DishWithId = ({match}) =>
+  {
+    return(
+        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+  };
 
   return (
    
@@ -40,6 +48,7 @@ class Main extends Component {
              <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path = '/menu/:dishId' component = {DishWithId} />
               <Route exact path = '/contactus' component = {Contact} /> 
               <Redirect to="/home" />
 
