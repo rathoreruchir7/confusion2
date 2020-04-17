d24cccdcac563743fc68972181dee2253270dd85
Ruchir Rathore <rathoreruchir7@gmail.com>
2020-04-17 15:34:14 +0530

    Combining Reducers

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import {Card, CardImg, CardText, CardImgOverlay, CardBody, CardTitle,Breadcrumb,BreadcrumbItem,
-	Button, Modal, ModalHeader, ModalBody, ModalFooter,Form,FormGroup,FormFeedback,Label,Input,Col,Row} from 'reactstrap';
-import {Link} from 'react-router-dom';
-// import CommentForm from './CommentForm';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter,Form,FormGroup,FormFeedback,Label,Input,Col,Row} from 'reactstrap';
 
+import {LocalForm,Control,Errors} from 'react-redux-form';
 const required = (val) => val && val.length;
 const minLength = (len) => (val) => val && (val.length >= len);
 
@@ -39,9 +37,9 @@ class CommentForm extends Component{
           touched: { ...this.state.touched, [field]: true },
         });
     }
-    handleSubmit(event)
+    handleSubmit(values)
     {
-        alert('Current State is: ' + JSON.stringify(this.state));
+        alert('Current State is: ' + JSON.stringify(values));
     }
     validate(name)
     {
@@ -107,92 +105,4 @@ class CommentForm extends Component{
         );
     }
 }
-
-
-	function RenderDish({dish})
-	{	return(
-		
-				<Card>
-					<CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                  <CardTitle>{dish.name}</CardTitle>
-                  <CardText>{dish.description}</CardText>
-                </CardBody>
-
-				</Card>
-				);
-	}
-
-	function RenderComments({comments})
-	{
-		const comment = comments.map((cmnt) => {
-			return(
-				
-				<ul className="list-unstyled" key={cmnt.id}>
-					<li>{cmnt.comment}</li>
-					<li>--{cmnt.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(cmnt.date)))}</li>
-
-				</ul>
-				
-			);
-		});
-		if(comment!=null){
-		return(
-
-			<div>
-				<h4>Comments</h4>
-			<div>
-				{comment}
-			</div>
-			<CommentForm />
-			</div>);
-		}
-		else
-		{
-			return(
-			<div></div>);
-		}
-	}
-	const Dishdetail = (props) => {
-		if(props.dish !=null){
-		return(
-			
-			<div className ="container">
-
-				<div className="row">
-                    <Breadcrumb>
-
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>                
-                </div>
-                
-				<div className="row">
-				<div className="col-12 col-md-5 m-1">	
-					<RenderDish dish={props.dish} />
-				</div>
-
-				<div className="col-12 col-md-5 m-1">
-				   <RenderComments comments = {props.comments} />
-				</div>
-			</div>
-			</div>
-				
-		);
-	}
-
-	else
-	{
-		return(
-			<div></div>
-			);
-	}
-
-	}
-
-
-export default Dishdetail; 
+export default CommentForm; 
